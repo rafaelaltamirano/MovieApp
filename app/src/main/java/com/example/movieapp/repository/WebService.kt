e98b0241cde387d321a84526793e6b8ed0ee064d
rafael altamirano <rafaelaltamirano_@outlook.com>
2022-01-13 00:51:49 -0300

    fiz web Service

--- app/src/main/java/com/example/movieapp/repository/WebService.kt
+++ app/src/main/java/com/example/movieapp/repository/WebService.kt
@@ -11,14 +11,14 @@ import retrofit2.http.Query
 
 // encargado de usar retrofit para traer la informacion del servidor  con los metodos get
 interface WebService {
-    @GET("movie/upcoming?")
+    @GET("movie/upcoming")
     //la query debe ser la misma que en la url, la query se denota con ? y luego el nombre en este caso api_key
     suspend fun getUpComingMovies(@Query("api_key") apiKey: String): MovieList
 
-    @GET("movie/top_rated?")
+    @GET("movie/top_rated")
     suspend fun getTopRatedMovie(@Query("api_key") apiKey: String): MovieList
 
-    @GET("movie/popular?")
+    @GET("movie/popular")
     suspend fun getPopularMovies(@Query("api_key") apiKey: String): MovieList
 }
 
